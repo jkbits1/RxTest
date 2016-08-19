ecbcfbe1afbcc32bb022e0cb4f0c62c69b418389
jkbits1 <jkbits1@users.noreply.github.com>
2016-08-19 01:12:45 +0100

    added comments, changed label names to be more descriptive

--- dotNet/RxTestsPractice/WinFormsApp45_NuGet/Form1.Designer.cs
+++ dotNet/RxTestsPractice/WinFormsApp45_NuGet/Form1.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelLength = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.labelCombined = new System.Windows.Forms.Label();
+            this.labelFiltered = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -42,25 +42,25 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(125, 15);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 5;
             // 
-            // label1
+            // labelLength
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 70);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 17);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "(Results)";
+            this.labelLength.AutoSize = true;
+            this.labelLength.Location = new System.Drawing.Point(16, 70);
+            this.labelLength.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelLength.Name = "labelLength";
+            this.labelLength.Size = new System.Drawing.Size(65, 17);
+            this.labelLength.TabIndex = 4;
+            this.labelLength.Text = "(Results)";
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(16, 15);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 3;
@@ -74,25 +74,24 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 6;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // label2
+            // labelCombined
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 145);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 17);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "(Results)";
+            this.labelCombined.AutoSize = true;
+            this.labelCombined.Location = new System.Drawing.Point(16, 145);
+            this.labelCombined.Name = "labelCombined";
+            this.labelCombined.Size = new System.Drawing.Size(65, 17);
+            this.labelCombined.TabIndex = 7;
+            this.labelCombined.Text = "(Results)";
             // 
-            // label3
+            // labelFiltered
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 207);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 17);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "(Results)";
+            this.labelFiltered.AutoSize = true;
+            this.labelFiltered.Location = new System.Drawing.Point(16, 207);
+            this.labelFiltered.Name = "labelFiltered";
+            this.labelFiltered.Size = new System.Drawing.Size(65, 17);
+            this.labelFiltered.TabIndex = 9;
+            this.labelFiltered.Text = "(Results)";
             // 
             // label4
             // 
@@ -132,13 +131,13 @@
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelFiltered);
+            this.Controls.Add(this.labelCombined);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelLength);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -149,11 +148,11 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelLength;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelCombined;
+        private System.Windows.Forms.Label labelFiltered;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
--- dotNet/RxTestsPractice/WinFormsApp45_NuGet/Form1.cs
+++ dotNet/RxTestsPractice/WinFormsApp45_NuGet/Form1.cs
@@ -33,46 +33,32 @@ namespace WinFormsApp45_NuGet
                         Select(evt => ((TextBox)evt.Sender).Text).Throttle(TimeSpan.FromSeconds(.2)).
                         DistinctUntilChanged();
 
-            //var input3 =
-            //        Observable.FromEventPattern(textBox3, "TextChanged").
-            //            Select(evt => ((TextBox)evt.Sender).Text).Throttle(TimeSpan.FromSeconds(.2)).
-            //            DistinctUntilChanged();
-
             var xs = from word in input.StartWith("")
                      from length in Task.Run(async () => { await Task.Delay(50); return word.Length; })
                      select length;
 
+            // text for length
             var res = xs.CombineLatest(clock, (len, now) => now.ToString() + " - Word length = " + len);
 
             res.ObserveOn(this).Subscribe(s =>
             {
-                //var s2 = input2;
-
-                label1.Text = s.ToString();
+                labelLength.Text = s.ToString();
             });
 
-            //var merged = input2.Merge(res);
-            //var merged = input2.CombineLatest(res, (s, s2) => "combined-" + s2 + " " + s);
+            // text for combined
             var merged = input2.CombineLatest(res, (s, s2) => s2 + " " + s);
 
-            //input2.ObserveOn(this).Subscribe(s2 => {
             merged.ObserveOn(this).Subscribe(s => {
-                label2.Text = s.ToString();
+                labelCombined.Text = s.ToString();
             });
 
+            // text for filtered
             var s2Filtered = merged.Where(s => s.Contains("s"));
 
             s2Filtered.ObserveOn(this).Subscribe(s =>
             {
-                label3.Text = 
-                    //"filtered on x -" + 
-                    s.ToString();
+                labelFiltered.Text = s.ToString();
             });
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
         }
     }
 }
